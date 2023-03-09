70ad8eaba56f8bf968482de7383eb6c8acc8ab43
Lance Roy <ldr709@gmail.com>
2023-03-09 17:30:22 +0100

    Define interface for polynomials over F_2

---
+++ avx2/polynomials.h
@@ -0,0 +1,49 @@
+#ifndef POLYNOMIALS_H
+#define POLYNOMIALS_H
+
+#include <inttypes.h>
+
+#include <immintrin.h>
+#include <wmmintrin.h>
+
+// Polys: 64, 128, 256, 512.
+// TODO: What about 192 and 384?
+
+typedef uint64_t poly64;
+typedef __m128i poly128;
+typedef __m256i poly256;
+typedef struct {
+	__m256i l;
+	__m256i h;
+} poly512;
+
+inline poly64 xor64(poly64 x, poly64 y)
+{
+	return x ^ y;
+}
+inline poly128 xor128(poly128 x, poly128 y)
+{
+	return _mm_xor_si128(x, y);
+}
+inline poly256 xor256(poly256 x, poly256 y)
+{
+	return _mm256_xor_si256(x, y);
+}
+
+inline poly128 mul64(poly64 x, poly64 y)
+{
+	return _mm_clmulepi64_si128(_mm_set_epi64x(0, x), _mm_set_epi64(0, y), 0x00)
+}
+
+inline poly256 mul128(poly128, poly128);
+inline poly512 mul256(poly256, poly256);
+
+// Reduction for implementing GF(2**n).
+inline poly64 reduce128_64(poly128 x);
+inline poly128 reduce256_128(poly256 x);
+inline poly256 reduce512_256(poly512 x);
+
+// TODO: Is it faster to try to combine multiplication and reduction for all GF(2**n)
+// multiplications, somehow?
+
+#endif
--- primitives.h
+++ primitives.h
@@ -1,52 +1,22 @@
 #ifndef PRIMITIVES_H
 #define PRIMITIVES_H
 
-# include <stddef.h>
-# include "config.h"
+#include <stddef.h>
+#include "config.h"
 
-# if defined(GALOIS_FIELD_CLMUL)
-
-#  include "field_clmul.h"
-
-// TODO: probably move below to field_clmul.h
-#  include <immintrin.h>
-#  include <wmmintrin.h>
-
-// TODO: 3 fields: 128, 196, 256 bits. Each with packed and unpacked versions.  Unpacked versions
-// are double length (or at least 64 bits longer, but double seems more useful), to avoid modular reduction.
-
-// TODO: Polys: 64(?), 128, 256, 512(?).
-
-typedef __m128i gf128;
-typedef __m256i gf256;
-
-inline gf128 xor_128(gf128 x, gf128 y)
-{
-	return _mm128_xor_si(x, y);
-}
-
-inline gf256 xor_256(gf256 x, gf256 y)
-{
-	return _mm256_xor_si256(x, y);
-}
-
-# elif defined(GALOIS_FIELD_C)
-
-// TODO
-
-# endif
+#include "polynomials.h"
 
 // TODO: Should probably use "blocks" much more often than "unsigned char*"s.
 
 
 // TODO: does this need to change based on even vs odd NIST levels?
 // TODO: ditch in favor of sizeof?
-# define DIGEST_SIZE (SECURITY_PARAM/4)
+#define DIGEST_SIZE (SECURITY_PARAM/4)
 
 // TODO: ought to pick a random oracle that allows parallelization, so that it's compatible if
 // anybody wants to make a parallel implementation.
-# if defined(RANDOM_ORACLE_SHA3)
-#  include "sha3.h"
+#if defined(RANDOM_ORACLE_SHA3)
+# include "sha3.h"
 
 // TODO
 
@@ -61,41 +31,15 @@ inline void random_oracle_final(random_oracle_state* ro, unsigned char* digest);
 inline random_oracle_digest random_oracle_digest_xor(random_oracle_digest a, random_oracle_digest b);
 
 // TODO: XOF?
-# endif
+#endif
 
-# if defined(AES_NI)
-#  include "aesni.h"
+#include "aes.h"
+#include "rijndael.h"
 
-// TODO: probably below definitions should go in aesni.h
-
-// AES, with key size == security parameter.
-typedef TODO aes_state;
-
-// Rijndael, with block size == key size == security parameter.
-typedef TODO rijndael_state;
-
-inline void aes_init(aes_state* cipher, const unsigned char* key);
-inline void aes_enc_block(const aes_state* cipher,
-                          unsigned char* ctxt, const unsigned char* msg);
-inline void aes_enc_pipelined(const aes_state* cipher,
-                              unsigned char* ctxt, const unsigned char* msg, size_t len);
-
-inline void rijndael_init(rijndael_state* cipher, const unsigned char* key);
-inline void rijndael_enc_block(const rijndael_state* cipher, unsigned char* ctxt,
-                               const unsigned char* msg);
-inline void rijndael_enc_pipelined(const rijndael_state* cipher,
-                                   unsigned char* ctxt, const unsigned char* msg, size_t len);
-
-# elif defined(AES_FIXSLICING)
-
-// TODO
-
-# endif
-
-# if defined(PRG_RIJNDAEL_EVEN_MANSOUR)
-#  include "even_mansour_impl.h"
-# elif defined(PRG_AES_CTR)
-#  include "ctr_impl.h"
-# endif
+#if defined(PRG_RIJNDAEL_EVEN_MANSOUR)
+#include "even_mansour_impl.h"
+#elif defined(PRG_AES_CTR)
+#include "ctr_impl.h"
+#endif
 
 #endif
