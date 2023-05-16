28f5f3f390c88945f38a9ab8aa29a7fdbc2df751
Lance Roy <ldr709@gmail.com>
2023-05-16 16:04:32 +0200

    Fix stack overflow

--- test/test_vector_com.cpp
+++ test/test_vector_com.cpp
@@ -16,9 +16,9 @@ extern "C" {
 
 TEST_CASE( "commit/open/verify", "[vector com]" ) {
     std::array<block_secpar, 2 * BITS_PER_WITNESS> roots = {0};
-    std::array<block_secpar, VECTOR_COMMIT_NODES> forest = {0};
-    std::array<block_secpar, VECTOR_COMMIT_LEAVES> leaves = {0};
-    std::array<block_2secpar, VECTOR_COMMIT_LEAVES> hashed_leaves = {0};
+    std::vector<block_secpar> forest(VECTOR_COMMIT_NODES);
+    std::vector<block_secpar> leaves(VECTOR_COMMIT_LEAVES);
+    std::vector<block_2secpar> hashed_leaves(VECTOR_COMMIT_LEAVES);
 
     // commit
     // XXX: this currently crashes
