07a95d97703f03d542540d9df10863ea342e6870
Lennart Braun <lenerd@posteo.de>
2023-05-23 13:21:42 +0200

    Extend vector commitment test (crashing)

--- test/test_vector_com.cpp
+++ test/test_vector_com.cpp
@@ -1,5 +1,6 @@
 
 #include <array>
+#include <vector>
 
 #include "test.hpp"
 
@@ -18,9 +19,18 @@ TEST_CASE( "commit/open/verify", "[vector com]" ) {
     std::array<block_secpar, 2 * BITS_PER_WITNESS> roots = {0};
     std::vector<block_secpar> forest(VECTOR_COMMIT_NODES);
     std::vector<block_secpar> leaves(VECTOR_COMMIT_LEAVES);
-    std::vector<block_2secpar> hashed_leaves(VECTOR_COMMIT_LEAVES);
+    std::vector<block_2secpar> hashed_leaves_sender(VECTOR_COMMIT_LEAVES);
+    std::vector<block_2secpar> hashed_leaves_receiver(VECTOR_COMMIT_LEAVES);
+    std::vector<unsigned char> delta(SECURITY_PARAM, 0);
+    std::vector<unsigned char> opening(VECTOR_OPEN_SIZE);
 
-    // commit
-    // XXX: this currently crashes
-    vector_commit(roots.data(), NULL, NULL, forest.data(), leaves.data(), hashed_leaves.data());
+    const auto hashed_leaves_sender_bytes = std::vector(reinterpret_cast<uint8_t*>(hashed_leaves_sender.data()),
+                                                        reinterpret_cast<uint8_t*>(hashed_leaves_sender.data() + hashed_leaves_sender.size()));
+    const auto hashed_leaves_receiver_bytes = std::vector(reinterpret_cast<uint8_t*>(hashed_leaves_receiver.data()),
+                                                          reinterpret_cast<uint8_t*>(hashed_leaves_receiver.data() + hashed_leaves_receiver.size()));
+
+    vector_commit(roots.data(), NULL, NULL, forest.data(), leaves.data(), hashed_leaves_sender.data());
+    vector_open(forest.data(), hashed_leaves_sender.data(), delta.data(), opening.data());
+    vector_verify(opening.data(), NULL, NULL, delta.data(), leaves.data(), hashed_leaves_receiver.data());
+    REQUIRE( hashed_leaves_receiver_bytes == hashed_leaves_sender_bytes );
 }
