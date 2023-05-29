jachiang/PQ-Threshold-Ring-Sigs-from-VOLEitH

commit 81fef07ba4402b6427f89f49229190c2f3fee4e1

Fix witness size check

faest.c
@@ -79,7 +79,7 @@ bool faest_compute_witness(secret_key* sk)
 			owf_block_xor(sk->pk.fixed_key.keys[0], owf_block_xor(owf_block_set_low32(i), sk->sk));
 #endif
 
-	for (unsigned int round = 1; round <= OWF_ROUNDS; ++round, w_ptr += sizeof(owf_block))
+	for (unsigned int round = 1; round <= OWF_ROUNDS; ++round)
 	{
 		for (uint32_t i = 0; i < OWF_BLOCKS; ++i)
 		{
@@ -101,7 +101,10 @@ bool faest_compute_witness(secret_key* sk)
 #endif
 
 			if (round < OWF_ROUNDS)
+			{
 				memcpy(w_ptr + i * sizeof(owf_block) * (OWF_ROUNDS - 1), &after_sbox, sizeof(owf_block));
+				w_ptr += sizeof(owf_block);
+			}
 		}
 	}
 

commit d1b7544fb6ab02cd5d8c9fac6cbbca484ed9fda0

Fix bug introduced in 81fef07ba4402b6427f89f49229190c2f3fee4e1

faest.c
@@ -101,11 +101,11 @@ bool faest_compute_witness(secret_key* sk)
 #endif
 
 			if (round < OWF_ROUNDS)
-			{
 				memcpy(w_ptr + i * sizeof(owf_block) * (OWF_ROUNDS - 1), &after_sbox, sizeof(owf_block));
-				w_ptr += sizeof(owf_block);
-			}
 		}
+
+		if (round < OWF_ROUNDS)
+			w_ptr += sizeof(owf_block);
 	}
 
 	assert(w_ptr - (uint8_t*) &sk->witness == WITNESS_BITS / 8);