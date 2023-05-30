a8745816049e8695d76bee3d590113d677ac4b8f
Lance Roy <ldr709@gmail.com>
2023-05-30 02:20:18 +0200

    Fix rijndael192_rotate_rows_undo_128

--- avx2/aes_impl.c
+++ avx2/aes_impl.c
@@ -468,10 +468,10 @@ ALWAYS_INLINE void rijndael192_rotate_rows_undo_128(block128* s)
 	block128 b1_blended = _mm_blendv_epi8(s[1], s[0], mask);
 
 	block128 shuffle_b0 = _mm_setr_epi8(
-		 0,  1,  2,  7,
-		 4,  5,  6, 15,
-		 8,  9, 10, 11,
-		12, 13, 14,  3);
+		 0,  1,  2, 11,
+		 4,  5,  6,  7,
+		 8,  9, 10,  3,
+		12, 13, 14, 15);
 	block128 shuffle_b1 = _mm_setr_epi8(
 		 0,  1,  2, 11,
 		 4,  5,  6,  7,
