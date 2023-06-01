0f381de7eeda8de7013504461a79910fcf335811
Lennart Braun <lenerd@posteo.de>
2023-06-01 03:47:14 +0200

    Add test vectors for faest aes-ctr variants

--- test/test_faest.cpp
+++ test/test_faest.cpp
@@ -1,10 +1,12 @@
 #include <array>
 
 #include "test.hpp"
+#include "test_faest_tvs.hpp"
 #include "test_witness.hpp"
 
 extern "C" {
 
+#include "config.h"
 #include "faest_details.h"
 
 }
@@ -73,3 +75,45 @@ TEST_CASE( "keygen/sign/verify", "[faest]" ) {
     REQUIRE( faest_sign(signature.data(), reinterpret_cast<const uint8_t*>(message.c_str()), message.size(), packed_sk.data(), NULL, 0) );
     REQUIRE( faest_verify(signature.data(), reinterpret_cast<const uint8_t*>(message.c_str()), message.size(), packed_pk.data()) );
 }
+
+
+#if defined(OWF_AES_CTR) && defined(TREE_PRG_AES_CTR) && defined(LEAF_PRG_SHAKE) && \
+    (((SECURITY_PARAM == 128) && (BITS_PER_WITNESS == 11 || BITS_PER_WITNESS == 16)) || \
+     ((SECURITY_PARAM == 192) && (BITS_PER_WITNESS == 16 || BITS_PER_WITNESS == 24)) || \
+     ((SECURITY_PARAM == 256) && (BITS_PER_WITNESS == 22 || BITS_PER_WITNESS == 32)))
+
+TEST_CASE( "test vector", "[faest]" ) {
+#if SECURITY_PARAM == 128 && BITS_PER_WITNESS == 11
+    namespace tv = faest_tvs::faest_128s_tvs;
+#elif SECURITY_PARAM == 128 && BITS_PER_WITNESS == 16
+    namespace tv = faest_tvs::faest_128f_tvs;
+#elif SECURITY_PARAM == 192 && BITS_PER_WITNESS == 16
+    namespace tv = faest_tvs::faest_192s_tvs;
+#elif SECURITY_PARAM == 192 && BITS_PER_WITNESS == 24
+    namespace tv = faest_tvs::faest_192f_tvs;
+#elif SECURITY_PARAM == 256 && BITS_PER_WITNESS == 22
+    namespace tv = faest_tvs::faest_256s_tvs;
+#elif SECURITY_PARAM == 256 && BITS_PER_WITNESS == 32
+    namespace tv = faest_tvs::faest_256f_tvs;
+#endif
+    using faest_tvs::message;
+
+    REQUIRE( tv::packed_sk.size() == FAEST_SECRET_KEY_BYTES );
+    REQUIRE( tv::packed_pk.size() == FAEST_PUBLIC_KEY_BYTES );
+    REQUIRE( tv::signature.size() == FAEST_SIGNATURE_BYTES );
+
+    std::array<uint8_t, FAEST_PUBLIC_KEY_BYTES> packed_pk;
+    std::array<uint8_t, FAEST_SIGNATURE_BYTES> signature;
+
+    faest_pubkey(packed_pk.data(), tv::packed_sk.data());
+    CHECK( packed_pk == tv::packed_pk );
+
+    REQUIRE( faest_sign(signature.data(), reinterpret_cast<const uint8_t*>(message.c_str()), message.size(), tv::packed_sk.data(), tv::randomness.data(), tv::randomness.size()) );
+    CHECK( signature == tv::signature );
+
+    std::cerr << signature << "\n";
+
+    REQUIRE( faest_verify(signature.data(), reinterpret_cast<const uint8_t*>(message.c_str()), message.size(), tv::packed_pk.data()) );
+}
+
+#endif
