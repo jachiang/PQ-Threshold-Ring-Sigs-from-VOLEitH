5517574788ecdf2a102d3c9f05d50b328cfaef86
James Chiang <james.chiangwu@gmail.com>
2024-08-12 08:10:38 +0000

    JC: Implement test - naive ring owf proof.

--- test/test_owf_proof.cpp
+++ test/test_owf_proof.cpp
@@ -35,3 +35,36 @@ TEST_CASE( "owf proof", "[owf proof]" ) {
 	auto [check_prover, check_verifier] = qs_test.compute_check();
     REQUIRE(check_prover == check_verifier);
 }
+
+
+TEST_CASE( "ring owf proof", "[ring owf proof]" ) {
+
+    // JC: generate pk-ring.
+    public_key_ring pk_ring;
+    int active_branch = 0;
+    secret_key sk;
+    for (int i; i < FAEST_RING_SIZE; ++i) {
+        std::array<uint8_t, FAEST_SECRET_KEY_BYTES> packed_sk;
+        std::array<uint8_t, FAEST_PUBLIC_KEY_BYTES> packed_pk;
+        test_gen_keypair(packed_pk.data(), packed_sk.data());
+        public_key pk;
+        faest_unpack_public_key(&pk, packed_pk.data());
+        if (i == active_branch) {
+            // JC: Expanded witness is stored in sk.
+            faest_unpack_secret_key(&sk, packed_sk.data());
+        }
+        pk_ring.pubkeys[i] = pk;
+    }
+
+    const auto delta = rand<block_secpar>();
+    // TODO: expand witness to accomodate active branch index as secret input.
+    quicksilver_test_or_state qs_test(OWF_NUM_CONSTRAINTS, reinterpret_cast<uint8_t*>(sk.witness), WITNESS_BITS, delta);
+    auto& qs_state_prover = qs_test.prover_state;
+    auto& qs_state_verifier = qs_test.verifier_state;
+
+    owf_constraints_prover_all_branches(&qs_state_prover, &pk_ring);
+    owf_constraints_verifier_all_branches(&qs_state_verifier, &pk_ring);
+
+	auto [check_prover, check_verifier] = qs_test.compute_check(active_branch);
+    REQUIRE(check_prover == check_verifier);
+}
