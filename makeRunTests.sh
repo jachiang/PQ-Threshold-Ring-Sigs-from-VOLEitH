c01f7a4e5045d35a2fac1630c0548aa4e0d73b4a
James Chiang <james.chiangwu@gmail.com>
2025-01-08 07:52:58 +0000

    Add build script

---
+++ makeRunTests.sh
@@ -0,0 +1,12 @@
+make -j4 all-sec128_cccs_11_7_102_avx2 &&
+make -j4 all-sec192_cccs_16_12_162_avx2 &&
+make -j4 all-sec256_cccs_22_6_245_avx2 &&
+make -j4 all-sec128_eccs_11_7_102_avx2 &&
+make -j4 all-sec192_eccs_16_12_162_avx2 &&
+make -j4 all-sec256_eccs_22_6_245_avx2 &&
+Additional_Implementations/sec128_cccs_11_7_102_avx2/./sec128_cccs_11_7_102_avx2_test &&
+Additional_Implementations/sec192_cccs_16_12_162_avx2/./sec192_cccs_16_12_162_avx2_test &&
+Additional_Implementations/sec256_cccs_22_6_245_avx2/./sec256_cccs_22_6_245_avx2_test &&
+Additional_Implementations/sec128_eccs_11_7_102_avx2/./sec128_eccs_11_7_102_avx2_test &&
+Additional_Implementations/sec192_eccs_16_12_162_avx2/./sec192_eccs_16_12_162_avx2_test &&
+Additional_Implementations/sec256_eccs_22_6_245_avx2/./sec256_eccs_22_6_245_avx2_test
